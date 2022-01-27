3456818457853ac3e9c4b97c65aa212ea0a41f6e
Eduardo <eduardomelle@gmail.com>
2022-01-27 19:40:56 -0300

    Continuing to implement the components of user service and the integration to department service.

--- src/main/java/com/dailycodebuffer/user/controller/UserController.java
+++ src/main/java/com/dailycodebuffer/user/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.dailycodebuffer.user.controller;
 
+import com.dailycodebuffer.user.VO.ResponseTemplateVO;
 import com.dailycodebuffer.user.entity.User;
 import com.dailycodebuffer.user.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +26,12 @@ public class UserController {
   public User saveUser(@RequestBody User user) {
     log.info("Inside saveUser method of UserController");
     return userService.saveUser(user);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId) {
+    log.info("Inside getUserWithDepartment method of UserController");
+    return userService.getUserWithDepartment(userId);
   }
 
 }
--- src/main/java/com/dailycodebuffer/user/service/UserService.java
+++ src/main/java/com/dailycodebuffer/user/service/UserService.java
@@ -1,10 +1,13 @@
 package com.dailycodebuffer.user.service;
 
+import com.dailycodebuffer.user.VO.Department;
+import com.dailycodebuffer.user.VO.ResponseTemplateVO;
 import com.dailycodebuffer.user.entity.User;
 import com.dailycodebuffer.user.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,9 +18,20 @@ public class UserService {
   @Autowired
   private UserRepository userRepository;
 
+  @Autowired
+  private RestTemplate restTemplate;
+
   public User saveUser(User user) {
     log.info("Inside saveUser method of UserService");
     return userRepository.save(user);
+  }
+
+  public ResponseTemplateVO getUserWithDepartment(Long userId) {
+    log.info("Inside getUserWithDepartment method of UserService");
+    User user = userRepository.findByUserId(userId);
+    Department department = restTemplate.getForObject("http://localhost:9001/departments/" + user.getDepartmentId(),
+        Department.class);
+    return new ResponseTemplateVO(user, department);
   }
 
 }
